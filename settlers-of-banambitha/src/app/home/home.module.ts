605038a08b981da9b76820ffb85744f79d15e8f3
AnyielFernandes <anyf23.5@gmail.com>
2021-03-21 17:59:29 +0100

    Añadidos componentes principales del modulo home

--- settlers-of-banambitha/src/app/home/home.module.ts
+++ settlers-of-banambitha/src/app/home/home.module.ts
@@ -9,11 +9,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select'; 
 import {MatDividerModule} from '@angular/material/divider'; 
 import {FlexModule} from '@angular/flex-layout';
+import { ShopComponent } from './shop/shop.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RulesComponent } from './rules/rules.component';
 
 
 
 @NgModule({
-  declarations: [PlayComponent, PlayerCardComponent],
+  declarations: [PlayComponent, PlayerCardComponent, ShopComponent, SettingsComponent, ProfileComponent, RulesComponent],
   imports: [
     CommonModule, 
     MatCardModule, 
