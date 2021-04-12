932fc4f46ae75bc287a76ff2d47b1c6287fe5c7a
jaimecb <jaime.conchello@gmail.com>
2021-04-12 21:05:42 +0200

    Esctructura pantalla de juego

--- settlers-of-banambitha/src/app/game/game.module.ts
+++ settlers-of-banambitha/src/app/game/game.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameplayComponent } from './gameplay/gameplay.component';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BoardComponent } from './board/board.component';
+import { ChatComponent } from './chat/chat.component';
+import { StatsComponent } from './stats/stats.component';
+import { PlayerInfoComponent } from './player-info/player-info.component';
+
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GameplayComponent, BoardComponent, ChatComponent, StatsComponent, PlayerInfoComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatTableModule,
+    MatListModule
+  ],
+  exports: [GameplayComponent]
 })
 export class GameModule { }
