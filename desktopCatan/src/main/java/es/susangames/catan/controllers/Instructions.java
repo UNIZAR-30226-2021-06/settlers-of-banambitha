addf4432a45199bb20eeeb78b2e875961fce03a0
jaimecb <jaime.conchello@gmail.com>
2021-04-14 21:07:51 +0200

    Añadida estructura de la pantalla de reglas

---
+++ desktopCatan/src/main/java/es/susangames/catan/controllers/Instructions.java
@@ -0,0 +1,35 @@
+package es.susangames.catan.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import javafx.scene.input.MouseEvent;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+import es.susangames.catan.App;
+
+public class Instructions {
+ 
+   
+    @FXML
+    private AnchorPane anchorPane;
+
+   
+
+   
+   
+   
+    public Instructions() {
+      
+    }
+
+
+    
+
+}
